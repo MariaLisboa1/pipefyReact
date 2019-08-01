d2f9720aa6f5f0fba0f57d71bd4d250168ff5daf
zaza.naza95 <zaza.naza95@gmail.com>
2019-08-01 09:43:11 -0300

    Projeto finalizado

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
+
+import BoardContext from '../Board/context';
 
 import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index, listIndex }) {
 	const ref = useRef();
+	const { move } = useContext(BoardContext);
 
 	const [{ isDragging }, dragRef] = useDrag({
-		item: { type: 'CARD', id: data.id },
+		item: { type: 'CARD', index, listIndex },
 		collect: monitor => ({
 			isDragging: monitor.isDragging()
 		})
@@ -16,15 +19,41 @@ export default function Card({ data }) {
 	const [, dropRef] = useDrop({
 		accept: 'CARD',
 		hover(item, monitor) {
-			console.log('hi');
-			console.log(item.id);
+			const draggedListIndex = item.listIndex;
+			const targetListIndex = listIndex;
+
+			const draggedIndex = item.index;
+			const targetIndex = index;
+
+			if (draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
+				return;
+			}
+
+			const targetSize = ref.current.getBoundingClientRect();
+			const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+			const draggedOffSet = monitor.getClientOffset();
+			const draggedTop = draggedOffSet.y - targetSize.top;
+
+			if (draggedIndex < targetIndex && draggedTop < targetCenter) {
+				return;
+			}
+
+			if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+				return;
+			}
+
+			move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+
+			item.index = targetIndex;
+			item.listIndex = targetListIndex;
 		}
 	});
 
 	dragRef(dropRef(ref));
 
 	return (
-		<Container ref={dragRef} isDragging={isDragging}>
+		<Container ref={ref} isDragging={isDragging}>
 			<header>{data.labels.map(label => <Label key={label} color={label} />)}</header>
 			<p>{data.content}</p>
 			{data.user && <img src="https://api.adorable.io/avatars/188/abott@adorable.png" alt="" />}
